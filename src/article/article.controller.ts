cd2c91eb9d68ba548909744bdebc6ad231ef0932
Vadim <vadim192000@gmail.com>
2022-11-08 23:04:42 +0300

    фикс сервиса статьи (генерируется рандомный slug) и правильно присвоен тип функции create

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -5,6 +5,7 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { brotliDecompressSync } from 'zlib';
 import { ArticleService } from './article.service';
 import { CreateArticleDto } from './dto/createArticle.dto';
+import { ArticleResponseInterface } from './types/articleResponse.interface';
 
 @Controller('articles')
 export class ArticleController {
@@ -15,7 +16,11 @@ export class ArticleController {
   async create(
     @User() currentUser: UserEntity,
     @Body('article') createArticleDto: CreateArticleDto,
-  ): Promise<any> {
-    return this.articleService.createArticle(currentUser, createArticleDto);
+  ): Promise<ArticleResponseInterface> {
+    const article = await this.articleService.createArticle(
+      currentUser,
+      createArticleDto,
+    );
+    return this.articleService.buildArticleResponse(article);
   }
 }
