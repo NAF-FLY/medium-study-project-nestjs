15da7e3c6242514f13c1ce069e120028100f5227
Vadim <vadim192000@gmail.com>
2022-11-09 16:43:52 +0300

    реализован лайк постов

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -37,4 +37,8 @@ export class UserEntity {
 
   @OneToMany(() => ArticleEntity, (article) => article.author)
   articles: ArticleEntity[];
+
+  @ManyToMany(() => ArticleEntity)
+  @JoinTable()
+  favorites: ArticleEntity[];
 }
